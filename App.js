08e82c166fd5d76b12df29c74bc417e204ec3c70
zedatgithub12 <96112790+zedatgithub12@users.noreply.github.com>
2023-02-14 14:19:59 +0300

    Feb 14 first

--- App.js
+++ App.js
@@ -56,58 +56,7 @@ import Events from "./Screens/Events";
 import BoughtDetail from "./Screens/BoughtTicketDetail";
 import Geolocation from "@react-native-community/geolocation";
 //import {requestUserPermission, NotificationListener} from './src/utils/pushnotification_helper';
-import PushNotification from "react-native-push-notification";
-
-// // Must be outside of any component LifeCycle (such as `componentDidMount`).
-// PushNotification.configure({
-//   // (optional) Called when Token is generated (iOS and Android)
-//   onRegister: function (token) {
-//     console.log("TOKEN:", token);
-//   },
-
-//   // (required) Called when a remote is received or opened, or local notification is opened
-//   onNotification: function (notification) {
-//     console.log("NOTIFICATION:", notification);
-
-//     // process the notification
-
-//     // (required) Called when a remote is received or opened, or local notification is opened
-//     notification.finish(PushNotificationIOS.FetchResult.NoData);
-//   },
-
-//   // (optional) Called when Registered Action is pressed and invokeApp is false, if true onNotification will be called (Android)
-//   onAction: function (notification) {
-//     console.log("ACTION:", notification.action);
-//     console.log("NOTIFICATION:", notification);
-
-//     // process the action
-//   },
-
-//   // (optional) Called when the user fails to register for remote notifications. Typically occurs when APNS is having issues, or the device is a simulator. (iOS)
-//   onRegistrationError: function(err) {
-//     console.error(err.message, err);
-//   },
-
-//   // IOS ONLY (optional): default: all - Permissions to register.
-//   permissions: {
-//     alert: true,
-//     badge: true,
-//     sound: true,
-//   },
-
-//   // Should the initial notification be popped automatically
-//   // default: true
-//   popInitialNotification: true,
-
-//   /**
-//    * (optional) default: true
-//    * - Specified if permissions (ios) and token (android and ios) will requested or not,
-//    * - if not, you must call PushNotificationsHandler.requestPermissions() later
-//    * - if you are not using remote notification or do not have Firebase installed, use this:
-//    *     requestPermissions: Platform.OS === 'ios'
-//    */
-//   requestPermissions: true,
-// });
+import {LocalNotification} from './src/Utils/localPushController';
 
 
 Geolocation.getCurrentPosition((info) => console.log(info.coords.latitude));
@@ -116,14 +65,9 @@ const persistor = persistStore(store);
 const prefix = Linking.createURL("com.afromina.placetobe://");
 
 export default function App() {
-
-// useEffect(()=>{
-//   requestUserPermission();
-//   NotificationListener();
-// }, [])
-
-
-
+const handleNotification =()=>{
+  LocalNotification();
+}
 
   // const [isLoading, setIsLoading] = React.useState(true);
   //const [userToken, setUserToken] = React.useState(null);
@@ -474,7 +418,7 @@ export default function App() {
         setConnectionState(true);
       }
     });
-
+    handleNotification();
     return () => {};
   }, [retry]);
 
---
+++ src/Utils/localPushController.js
@@ -0,0 +1,36 @@
+import PushNotification, { Importance } from 'react-native-push-notification';
+
+PushNotification.configure({
+    onNotification: function(notification){
+        console.log('Local Notification', notification);
+    },
+    popInitialNotification: true,
+    requestPermissions: true,
+});
+PushNotification.createChannel({
+    channelId: "channel-id", // (required)
+    channelName: "My channel", // (required)
+    channelDescription: 'A channel is for notification',
+    playSound: true,
+    soundName: "default",
+    importance: Importance.HIGH,
+    vibrate:true,
+}, 
+(created)=> console.log(`channel created '${created}'`),
+);
+
+export const LocalNotification =()=>{
+    PushNotification.localNotification({
+        channelId: 'channel-id',
+        channelName: 'my channel',
+        autoCancel: true,
+        bigText: 'This is for local notification',
+        subText: 'today',
+        message: 'hello from other side!!!',
+        playSound: true,
+        soundName: 'default',
+        importance: 10,
+        vibrate:true,
+        vibration: 400,
+    });
+}
